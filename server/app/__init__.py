5583d6f243f06832a8220040dbadb9bf0a4c0d22
Jean Luc <jeanluc.xii.iv@gmail.com>
2025-03-06 14:50:43 +0300

    integrate the JWTManager config into server/app/__init__.py

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mimir-database.db'
+app.config['JWT_SECRET_KEY'] = 'chn0531jea0412'
+
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 from app import models
 
