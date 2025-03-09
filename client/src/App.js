d84c480c6117deeff3c48f3c12e51fea5b45a5fc
Jean Luc <jeanluc.xii.iv@gmail.com>
2025-03-09 18:57:29 +0300

    create Home.js, Login.js, Register.js and register the routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <div>
       <h1>Welcome to Mimir</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
