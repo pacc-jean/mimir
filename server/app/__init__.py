abb8598d4459ab706743dfcfc728900c5f50df22
Jean Luc <jeanluc.xii.iv@gmail.com>
2025-03-09 20:19:20 +0300

    configure CORS

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mimir-database.db'
 app.config['JWT_SECRET_KEY'] = 'chn0531jea0412'
 
